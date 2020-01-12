8606a26c50b234bb7c5fdeface397301b5d5fe32
Sergey Li <pincats@gmail.com>
2020-01-12 19:55:24 +0300

    feat: List topics by ListView

--- app/src/main/java/com/example/android/fluentcast/MainActivity.java
+++ app/src/main/java/com/example/android/fluentcast/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.fluentcast;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("People");
+        topics.add("Travelling");
+        topics.add("Leisure");
+        topics.add("At home");
+        topics.add("Technologies");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, topics);
+        ListView view = findViewById(R.id.topic_list);
+        view.setAdapter(arrayAdapter);
+
     }
 }
